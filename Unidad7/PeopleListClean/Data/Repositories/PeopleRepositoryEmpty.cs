a562253f8538004d6cf36f13909c018468c21958
anamontero14 <ana.montero@iesnervion.es>
2025-11-11 19:28:30 +0100

    Cambios

---
+++ Unidad7/PeopleListClean/Data/Repositories/PeopleRepositoryEmpty.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Domain.UseCases.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class PeopleRepositoryEmpty : IPeopleRepository
+    {
+        /// <summary>
+        /// Método para agarrar todas las personas excepto que la lista de las
+        /// personas en este caso estará vacía para realizar pruebas
+        /// </summary>
+        /// <returns></returns>
+        public List<Persona> getPersonas()
+        {
+            return new List<Persona>
+            {
+            };
+
+        }
+    }
+}
---
+++ Unidad7/PeopleListClean/Domain/UseCases/PeopleListUseCase.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+using Domain.UseCases.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.UseCases
+{
+    public class PeopleListUseCase : IPeopleListUseCase
+    {
+        //se crea una variable que almacenara el listado de las personas
+        private readonly IPeopleRepository _peopleListRepository;
+
+        //Inyectamos en el constructor el repositorio
+        public PeopleListUseCase(IPeopleRepository peopleRepository)
+        {
+            _peopleListRepository = peopleRepository;
+        }
+
+        /// <summary>
+        /// Método que se implementa de la interfaz
+        /// </summary>
+        /// <returns></returns>
+        public List<Persona> getPersonasFiltradas() {
+
+            List<Persona> personasFiltradas = new List<Persona>();
+
+            foreach (Persona persona in _peopleListRepository.getPersonas()) {
+
+                if (persona.Edad > 18) {
+                    personasFiltradas.Add(persona);
+                }
+
+            }
+
+            return personasFiltradas;
+
+        }
+
+    }
+}
