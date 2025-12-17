93f5fa3fade3d6af7711fa1856df97e84cf18e97
anamontero14 <ana.montero@iesnervion.es>
2025-12-17 17:12:14 +0100

    Cambios

--- Unidad10/Ejercicio3/CompositionRoot/obj/Debug/net8.0/CompositionRoot.AssemblyInfo.cs
+++ Unidad10/Ejercicio3/CompositionRoot/obj/Debug/net8.0/CompositionRoot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompositionRoot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac42df66e865abd1d3664300dbfcb18fefd718a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847fe59ac1f6e55ddcb9b62be92a808cf01170cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompositionRoot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompositionRoot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
